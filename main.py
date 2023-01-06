3b661a38e89d8403e60253e62fac5e08e465a10b
MundaneMelon <hondow1234@gmail.com>
2023-01-06 12:11:15 -0700

    Flappy bird project completed

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 def main():
   print(
-'''Welcome to my really cool github repo.
+'''
+
+Welcome to my really cool github repo.
 This is mostly just dumb projects for school, or stuff I'm working on myself
 It's probably a bad idea to keep it all under one repo
 
 Ok that's it
 ''')
-
+  input("\n\n\nPress enter to exit program")
 if __name__ == "__main__":
   main()
