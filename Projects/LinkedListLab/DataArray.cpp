1056eb195f655181052bc26d97d9e3b235b0a15b
MundaneMelon <hondow1234@gmail.com>
2023-04-17 15:34:00 -0600

    I'm so incredibly confused wtf

--- Projects/LinkedListLab/DataArray.cpp
+++ Projects/LinkedListLab/DataArray.cpp
@@ -2,10 +2,19 @@
 // Created by mikeb on 3/25/2023.
 //
 
+#include <cstring>
 #include "DataArray.h"
-#include <iostream>
 
-DataArray::DataArray()
-{
-    node_array = [3000];
+DataArray::DataArray(Node n) {
+    Node* temp_n = &n;
+    int i = 0;
+    while(temp_n) {
+        node_array[i] = *temp_n;
+        temp_n = temp_n->head;
+        i++;
+    }
+    last = i - 1;
+}
+DataArray::DataArray() {
+    last = 0;
 }
--- Projects/LinkedListLab/DataArray.h
+++ Projects/LinkedListLab/DataArray.h
@@ -2,20 +2,26 @@
 // Created by mikeb on 3/25/2023.
 //
 
-#include "Node.cpp"
 
 #ifndef LINKEDLISTLAB_DATAARRAY_H
 #define LINKEDLISTLAB_DATAARRAY_H
 
+#include "Node.cpp"
+#include<iostream>
 
 class DataArray {
-
 private:
-    Node node_array [3000];
+    Node node_array[3000];
+    int last;
+    std::string lower(std::string s);
 public:
-    DataArray(Node* newFirstNode);
     DataArray();
-    ~DataArray();
+    explicit DataArray(Node n);
+    Node read(int index);
+    int indexOf(std::string val);
+    void insert(std::string data);
+    void remove(std::string val);
+    void sort();
 };
 
 
