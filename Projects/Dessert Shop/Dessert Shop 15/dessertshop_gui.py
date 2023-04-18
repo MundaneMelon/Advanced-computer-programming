5a5e58c236cf96520079af09b38f0bf156066877
MundaneMelon <hondow1234@gmail.com>
2023-04-18 12:10:53 -0600

    Update dessertshop_gui.py

--- Projects/Dessert Shop/Dessert Shop 15/dessertshop_gui.py
+++ Projects/Dessert Shop/Dessert Shop 15/dessertshop_gui.py
@@ -2,19 +2,21 @@ from desserts import *
 import tkinter
 
 def gui_main():
+    order = Order([])
     window = tkinter.Tk()
     window.title("Dessert Shop")
     window.geometry("300x200")
     window.configure(background="white")
-    screen1(window)
+    screen1(window, order)
 
 
-def screen1(window):
+
+def screen1(window, order):
     refresh(window)
     l = tkinter.Label(window, text = "What would you like to add to the order?",)
     l.config(font = ("Courier", 9))
     l.pack()
-    b1 = tkinter.Button(window, text = "Candy", command = lambda : candy_screen(window)).pack()
+    b1 = tkinter.Button(window, text = "Candy", command = lambda : candy_screen(window, order)).pack()
     b2 = tkinter.Button(window, text = "Cookie", ).pack()
     b3 = tkinter.Button(window, text = "Ice Cream", ).pack()
     b4 = tkinter.Button(window, text = "Sundae", ).pack()
@@ -22,11 +24,11 @@ def screen1(window):
     T = tkinter.Text(window, height=2, width=20, )
     window.mainloop()
 
-def candy_screen(window):
+def candy_screen(window, order):
     refresh(window)
     l1 = tkinter.Label(window, text = "What type of candy would you like to order?").pack()
     i1 = tkinter.Text(window, height=1, width=35).pack()
-    b1 = tkinter.Button(window, text="I want to go back :(", command= lambda : screen1(window)).pack()
+    b1 = tkinter.Button(window, text="I want to go back :(", command= lambda : screen1(window, order)).pack()
     window.mainloop()
 
 def refresh(window):
